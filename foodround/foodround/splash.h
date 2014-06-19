ea3dcca1a622c7c82cec7476c23ed77aaef408d3
Devine Lu Linvega <me@m0oo.com>
2014-06-19 17:02:16 -0400

    Splash

---
+++ foodround/foodround/splash.h
@@ -0,0 +1,15 @@
+//
+//  splash.h
+//  Ledoliel
+//
+//  Created by Devine Lu Linvega on 2014-06-19.
+//  Copyright (c) 2014 XXIIVV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface splash : UIViewController
+@property (strong, nonatomic) IBOutlet UIButton *splashSkipButton;
+- (IBAction)splashSkipButton:(id)sender;
+
+@end
